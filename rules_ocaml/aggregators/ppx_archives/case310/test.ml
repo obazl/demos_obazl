29cffd0c48d0a0823c30457ef49e9d30c7dd1908
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2022-06-29 21:27:40 -0500

    clean up ppx aggregators test cases

--- rules_ocaml/aggregators/ppx_archives/case310/test.ml
+++ rules_ocaml/aggregators/ppx_archives/case310/test.ml
@@ -1,8 +1,8 @@
 open OUnit2
 
-let test1 test_ctxt = assert_equal "255" [%red   255]
-let test2 test_ctxt = assert_equal "127" [%green 127]
-let test3 test_ctxt = assert_equal "80"  [%blue   80]
+let test1 _ = assert_equal "255" [%red   255]
+let test2 _ = assert_equal "127" [%green 127]
+let test3 _ = assert_equal "80"  [%blue   80]
 
 let suite =
 "suite">:::
--- rules_ocaml/aggregators/ppx_libraries/case424/test.ml
+++ rules_ocaml/aggregators/ppx_libraries/case424/test.ml
@@ -1,20 +1,20 @@
 open OUnit2
 
-let test1 test_ctxt = assert_equal 255 [%red   255]
-let test2 test_ctxt = assert_equal 127 [%green 127]
-let test3 test_ctxt = assert_equal 80  [%blue   80]
+let test1 _ = assert_equal 255 [%red   255]
+let test2 _ = assert_equal 127 [%green 127]
+let test3 _ = assert_equal 80  [%blue   80]
 
-let test4 test_ctxt = assert_equal "255" [%alpha   255]
-let test5 test_ctxt = assert_equal "127" [%beta    127]
-let test6 test_ctxt = assert_equal "80"  [%gamma    80]
+let test4 _ = assert_equal "255" [%alpha   255]
+let test5 _ = assert_equal "127" [%beta    127]
+let test6 _ = assert_equal "80"  [%gamma    80]
 
-let test7 test_ctxt = assert_equal 255 Main.r
-let test8 test_ctxt = assert_equal 127 Main.g
-let test9 test_ctxt = assert_equal 80  Main.b
+let test7 _ = assert_equal 255 Main.r
+let test8 _ = assert_equal 127 Main.g
+let test9 _ = assert_equal 80  Main.b
 
-let test10 test_ctxt = assert_equal "255" Main.alpha
-let test11 test_ctxt = assert_equal "127" Main.beta
-let test12 test_ctxt = assert_equal  "80" Main.gamma
+let test10 _ = assert_equal "255" Main.alpha
+let test11 _ = assert_equal "127" Main.beta
+let test12 _ = assert_equal  "80" Main.gamma
 
 let suite =
 "suite">:::
