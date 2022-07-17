2d3511261df5969653ee2d519412aa0adc52d0f6
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2022-07-17 00:50:08 -0500

    new tests and fixes in tools_obazl/convert

--- tools_obazl/convert/rules/work.scm
+++ tools_obazl/convert/rules/work.scm
@@ -1,11 +1,58 @@
-
 (define arg
   ;; "rules/with-stdout-to/cat/literal_deps"
-  ;; "rules/with-stdout-to/cat/glob"
+  "rules/with-stdout-to/cat/glob"
   ;; "rules/with-stdout-to/run"
-  "rules/install/run"
+
+  ;; "rules/install/run"
   ;; "rules/install/run/mypgm"
   )
+
+(let* ((_ (load "starlark.scm"))
+       (arg
+        ;; "multilibs/mwe/twolibs"
+
+        "rules/copy"
+
+        ;; "rules/progn/empties"
+
+        ;; "rules/with-stdout-to/cat/literal_deps"
+        ;; "rules/with-stdout-to/cat/tagged_deps"
+        ;; "rules/with-stdout-to/cat/glob"
+        ;; "rules/with-stdout-to/run"
+        ;; "rules/with-stdout-to/bash"
+        ;; "rules/with-stdout-to/chdir"
+
+        ;; "rules/write-file"
+
+        ;; "rules/install/run"
+        ;; "rules/install/run/mypgm"
+        )
+       (_wss (load-dune arg))
+       (@ws (assoc-val :@ _wss))
+       (pkgs (car (assoc-val :pkgs @ws)))
+       ;; ) pkgs)
+
+       ;; miblize
+       (npkgs (map (lambda (kv)
+                     (let ((mibl-pkg (dune-pkg->mibl :@ (cdr kv))))
+                       (hash-table-set! pkgs (car kv) mibl-pkg)))
+                   pkgs))
+       ;; ) npkgs)
+
+       ;; resolve stuff
+       (_ (resolve-labels (assoc-val :@ -mibl-ws-table)))
+       (_ (car (assoc-val :exports (assoc-val :@ _wss))))
+       ;; ) '())
+
+       )
+  ;; to starlark
+  (for-each (lambda (kv)
+              (mibl-pkg->starlark (cdr kv))
+              )
+            npkgs)
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define _wss
   (let* ((_ (load "starlark.scm"))
