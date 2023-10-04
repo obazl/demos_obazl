8d925da54ff6790d3cfbcc2560141b9d1e71ddd1
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2023-10-04 11:45:15 -0500

    update rules_ocaml demos

--- rules_ocaml/interop/ffi/case110/cclibs/alpha.ml
+++ rules_ocaml/interop/ffi/case110/cclibs/alpha.ml
@@ -1,5 +1,7 @@
 (* for demo purposes, we put the wrapper in the same package as the C library. *)
 
+(* Note that we do not use anything from the OCaml C API *)
+
 external my_int_fn: unit -> int = "caml_my_int_fn"
 external my_str_fn: unit -> string = "caml_my_str_fn"
 
